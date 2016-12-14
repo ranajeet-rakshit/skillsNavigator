15f2ec5313d2c5d790d8734b17187d28d20cf13b
ranajeet-rakshit <ranajeetster@gmail.com>
2016-12-15 00:48:15 +0530

    Integrated UI with backend service. Removed Static data with DB table data.

--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -8,7 +8,29 @@
  * Controller of the skillsNavigatorApp
  */
 angular.module('skillsNavigatorApp')
-  .controller('ServicesCtrl', function () {
+  .controller('ServicesCtrl', function ($scope, DataService) {
+
+  	var tables='about||what_we_do||pmkvy||why_skills_nav||pmkvy_charecteristics||pmkvy_char_points';
+
+
+    DataService.getTables(tables).
+      then(getTablesSuccess, getTablesError);
+
+    function getTablesSuccess(res){
+    	console.log(res);
+    	$scope.about = res.about;
+    	$scope.services = res.what_we_do;
+    	$scope.pmkvy = res.pmkvy[0];
+    	$scope.whySkillsNav = res.why_skills_nav;
+    	$scope.pmkvy_overview = res.pmkvy_charecteristics;
+    	$scope.pmkvyCharPoints = res.pmkvy_char_points;
+    }
+
+    function getTablesError(err){
+    	console.log(err);
+    }
+
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
